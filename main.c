5a3472817f6abf6e69bea2241e53680951eaab7d
agozlan <agozlan@student.42.fr>
2025-08-28 18:38:55 +0200

    Add tentative parsing ARP request

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include "malcolm.h"
+#include <linux/if_ether.h>
 
 int g_signal = 0;
+
+struct arp_eth_ipv4 {
+  unsigned short int ar_hrd;
+  unsigned short int ar_pro;
+  unsigned char ar_hln;
+  unsigned char ar_pln;
+  unsigned short int ar_op;
+  unsigned char ar_sha[ETH_ALEN]; // Sender hardware address
+  unsigned char ar_sip[4];        // Sender IP address
+  unsigned char ar_tha[ETH_ALEN]; // Target hardware address
+  unsigned char ar_tip[4];        // Target IP address
+};
 
 void ctrlC(int signum) {
   (void)signum;
@@ -38,11 +51,31 @@ int main(int argc, char **argv) {
     }
     printf("Reçu un paquet ARP de %d octets\n", numbytes);
     printf("\n\nDetail:\n\n%s\n", buffer);
+    break; // pour tester deja 1 request
     // ecouter les signaux
     if (g_signal == 1)
       break;
   }
 
+  struct ethhdr *rcv_resp = (struct ethhdr *)buffer;
+  struct arp_eth_ipv4 *arp_resp =
+      (struct arp_eth_ipv4 *)(buffer + sizeof(struct ethhdr));
+
+  if (ntohs(rcv_resp->h_proto) != ETH_P_ARP) { // pas certain d'en avoir besoin
+    printf("Soucis h h_proto\n");
+    return (0);
+  }
+
+  printf("arp sender ip address = %d.%d.%d.%d\n", arp_resp->ar_sip[0],
+         arp_resp->ar_sip[1], arp_resp->ar_sip[2], arp_resp->ar_sip[3]);
+  printf("arp target ip address = %d.%d.%d.%d\n", arp_resp->ar_tip[0],
+         arp_resp->ar_tip[1], arp_resp->ar_tip[2], arp_resp->ar_tip[3]);
+
+  /*
+struct in_addr sender_a;
+memset(&sender_a, 0, sizeof(struct in_addr));
+memcpy(&sender_a.s_addr, arp_resp->sender_ip, sizeof(uint32_t));
+*/
   close(sockfd);
   return (0);
 }
