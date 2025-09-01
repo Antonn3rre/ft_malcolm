74b61b00c430ef0ae8a3b6bdee7fc5ed483813b7
agozlan <agozlan@student.42.fr>
2025-09-01 11:54:57 +0200

    FInd interface (manque recup MAC et IP)

--- checkInterface.c
+++ checkInterface.c
@@ -1,12 +1,19 @@
 #include "malcolm.h"
+// #include <cstdint>
 
 int checkInterface(struct s_input *input) {
 
   (void)input;
   struct ifaddrs *ifap;
 
+  // Get interfaces list
   if (getifaddrs(&ifap) == -1)
     return (perror("getifaddr"), 0);
+
+  uint32_t inp_tip, inp_sip;
+  ft_memcpy(&inp_tip, input->in_tip, 4);
+  ft_memcpy(&inp_sip, input->in_sip, 4);
+
   while (ifap) {
 
     if (!ifap->ifa_addr || ifap->ifa_addr->sa_family != AF_INET) {
@@ -16,16 +23,16 @@ int checkInterface(struct s_input *input) {
     struct sockaddr_in *addr = (struct sockaddr_in *)ifap->ifa_addr;
     struct sockaddr_in *netmask = (struct sockaddr_in *)ifap->ifa_netmask;
 
-    uint32_t ip_given, ip_iface, mask;
+    uint32_t ip_iface, mask;
 
-    ft_memcpy(&ip_given, input->in_tip, 4);
+    ip_iface = addr->sin_addr.s_addr;
+    mask = netmask->sin_addr.s_addr;
 
-    ip_iface = ntohl(addr->sin_addr.s_addr);
-    mask = ntohl(netmask->sin_addr.s_addr);
-
-    printf("Test interface %s: ip_given=%08x ip_iface=%08x mask=%08x\n",
-           ifap->ifa_name, ip_given, ip_iface, mask);
-    if ((ip_given & mask) == (ip_iface & mask)) {
+    printf("inp_tip = %u, inp_sip = %u, ip_iface = %u, mask = %u\n", inp_tip,
+           inp_sip, ip_iface, mask);
+    if ((inp_tip & mask) == (ip_iface & mask) &&
+        (inp_sip & mask) == (ip_iface & mask)) {
+      // TODO: reboucler pour trouver mac
       printf("Found interface: %s\n", ifap->ifa_name);
       ft_memcpy(input->interface, ifap->ifa_name, ft_strlen(ifap->ifa_name));
       input->interface[ft_strlen(ifap->ifa_name)] = 0;
